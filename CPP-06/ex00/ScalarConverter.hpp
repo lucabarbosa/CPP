cc9cce85aca5777c7271a2e657e39475b0ee798e
Luca Barbosa <lucabento.luca@gmail.com>
2026-05-22 18:07:17 -0300

    Creating CPP06

---
+++ CPP-06/ex00/ScalarConverter.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbento <lbento@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/22 18:03:12 by lbento            #+#    #+#             */
+/*   Updated: 2026/05/22 18:05:27 by lbento           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter
+{
+	private:
+
+	public:
+		ScalarConverter(/* args */);
+		~ScalarConverter();
+};
+
+
+#endif
