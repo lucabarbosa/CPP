69bfbb5654f418a326bf6159753a02c1ea0e84bf
lucabarbosa <lucabento.luca@gmail.com>
2026-06-25 20:50:16 -0300

    Error finding while compiling in macos arm 64

--- CPP-07/ex02/main.cpp
+++ CPP-07/ex02/main.cpp
@@ -6,14 +6,61 @@
 /*   By: lbento <lbento@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/24 17:44:05 by lbento            #+#    #+#             */
-/*   Updated: 2026/06/25 00:52:19 by lbento           ###   ########.fr       */
+/*   Updated: 2026/06/25 20:49:01 by lbento           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-int   main(void)
+#define MAX_VAL 750
+
+int   main(int, char **)
 {
-   
-   return (0);
+	{
+		std::cout << "\033[3;34mSubject Tests\033[0m" << std::endl;
+		Array<int>	numbers(MAX_VAL);
+		int*	mirror = new int[MAX_VAL];
+		srand(time(NULL));
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			const int	value = rand();
+			numbers[i] = value;
+			mirror[i] = value;
+		}
+		//SCOPE
+		{
+			Array<int> tmp = numbers;
+			Array<int> test(tmp);
+		}
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			if(mirror[i] != numbers[i])
+			{
+				std::cerr << "didn't save the same value!!" << std::endl;
+				return 1;
+			}
+		}
+		try
+		{
+			numbers[-2] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		try
+		{
+			numbers[MAX_VAL] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			numbers[i] = rand();
+		}
+		delete [] mirror;
+	}
+	return (0);
 }
